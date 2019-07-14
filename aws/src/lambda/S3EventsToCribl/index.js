206547fb6df3fb61f852306de3c2fd2af293c76e
robmarlow <35861815+robmarlow@users.noreply.github.com>
2019-07-13 23:49:40 -0500

    update to regex and field extration for alb (#1)

--- aws/src/lambda/S3EventsToCribl/index.js
+++ aws/src/lambda/S3EventsToCribl/index.js
@@ -46,9 +46,9 @@ const ELB_PARSE_REGEX = /^(\d\S+)\s+(\S+)\s+([\d.]+):(\d+)\s+([\d.]+):(\d+)\s+([
 const ELB_FIELDS = ['timestamp', 'elb', 'client_ip', 'client_port', 'backend_ip', 'backend_port', 'request_processing_time', 'backend_processing_time', 'response_processing_time', 'elb_status_code', 'backend_status_code', 'received_bytes', 'sent_bytes', 'request', 'user_agent', 'ssl_cipher', 'ssl_protocol'];
 
 // e.g https 2018-07-02T22:23:00.186641Z app/my-loadbalancer/50dc6c495c0c9188 192.168.131.39:2817 10.0.0.1:80 0.086 0.048 0.037 200 200 0 57 "GET https://www.example.com:443/ HTTP/1.1" "curl/7.46.0" ECDHE-RSA-AES128-GCM-SHA256 TLSv1.2 arn:aws:elasticloadbalancing:us-east-2:123456789012:targetgroup/my-targets/73e2d6bc24d8a067 "Root=1-58337281-1d84f3d73c47ec4e58577259" "www.example.com" "arn:aws:acm:us-east-2:123456789012:certificate/12345678-1234-1234-1234-123456789012" 1 2018-07-02T22:22:48.364000Z "authenticate,forward" "-"
-const ALB_PARSE_REGEX = /^([a-zA-Z]\S+)\s+(\S+)\s+(\S+)\s+([\d.]+):(\d+)\s+([\d.]+):(\d+)\s+([\d.]+)\s+([\d.]+)\s+([\d.]+)\s+([\d-]+)\s+([\d-]+)\s+(\d+)\s+(\d+)\s+"([^"]+)"\s+"([^"]+)"\s+([-\w]+)\s+([-\w.]+)\s+(\S+)\s+"([^"]+)"\s+"([^"]+)"\s+"([^"]+)"\s+([-\d]+)\s+(\S+)\s+"([^"]+)"\s+"([^"]+)"$/;
+const ALB_PARSE_REGEX = /([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) ([^ ]*)[:-]([0-9]*) ([-.0-9]*) ([-.0-9]*) ([-.0-9]*) (|[-0-9]*) (-|[-0-9]*) ([-0-9]*) ([-0-9]*) \"([^ ]*) ([^ ]*) (- |[^ ]*)\" \"([^\"]*)\" ([A-Z0-9-]+) ([A-Za-z0-9.-]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([-.0-9]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\"($| \"[^ ]*\")/;
 // from: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html
-const ALB_FIELDS = ['type', ...ELB_FIELDS, 'backend_group_arn', 'trace_id', 'domain_name', 'chosen_cert_arn', 'matched_rule_priority', 'request_creation_time', 'actions_executed', 'redirect_url'];
+const ALB_FIELDS = ['type', 'timestamp', 'elb', 'client_ip', 'client_port', 'target_ip', 'target_port', 'request_processing_time', 'target_processing_time', 'response_processing_time', 'elb_status_code', 'target_status_code', 'received_bytes', 'sent_bytes', 'request_verb', 'request_url', 'request_proto', 'user_agent', 'ssl_cipher', 'ssl_protocol', 'target_group_arn', 'trace_id', 'domain_name', 'chosen_cert_arn', 'matched_rule_priority', 'request_creation_time', 'actions_executed', 'redirect_url', 'error_reason'];
 
 
 // eg. E1VXRJJS5KC0JK.2018-11-20-23.0128d8dc.gz
@@ -62,7 +62,7 @@ function asNumberIfPossible(val, decode) {
 function lineToEvent(line, delim, fields, decode) {
   const parts = line.split(delim);
   const event = {};
-  for (let p=0; p<parts.length && p<fields.length; p++) {
+  for (let p = 0; p < parts.length && p < fields.length; p++) {
     const val = parts[p];
     event[fields[p]] = asNumberIfPossible(val, decode);
   }
@@ -79,13 +79,13 @@ function cloudfrontUrlDecode(val) {
     return val;
   }
   // a few chars are double uri encoded ... WIN!!!
-  return decodeURIComponent(val).replace(/%(20|22|5C)/g, function(val) {
-    if(val === '%20') return ' ';
-    if(val === '%22') return '"';
-    if(val === '%5C') return '\\';
+  return decodeURIComponent(val).replace(/%(20|22|5C)/g, function (val) {
+    if (val === '%20') return ' ';
+    if (val === '%22') return '"';
+    if (val === '%5C') return '\\';
     return val;
   });
-} 
+}
 
 function parseEvents(fullKey, buf) {
   let p = Promise.resolve(buf);
@@ -108,7 +108,7 @@ function parseEvents(fullKey, buf) {
       const DELIM = ' ';
       const lines = data.toString().split(/\r?\n/);
       const header = lines[0].split(DELIM).map(normalizeFieldName);
-      for (let i=1; i<lines.length; i++) {
+      for (let i = 1; i < lines.length; i++) {
         const line = lines[i].trim();
         if (line.length) {
           const event = lineToEvent(line, DELIM, header);
@@ -134,11 +134,11 @@ function parseEvents(fullKey, buf) {
         return Promise.reject(new Error(`Failed to find valid CDN header line, key=${fullKey}`));
       }
       const header = lines[1].substr('#Fields: '.length).split(' ').map(normalizeFieldName);
-      for (let i=2; i<lines.length; i++) {
+      for (let i = 2; i < lines.length; i++) {
         const line = lines[i].trim();
         if (line.length) {
           const event = lineToEvent(line, DELIM, header, cloudfrontUrlDecode);
-          event._time = Date.parse(`${event.date} ${event.time} GMT`)/1000;
+          event._time = Date.parse(`${event.date} ${event.time} GMT`) / 1000;
           event._raw = line;
           event.source = fullKey;
           event.origin = 'lambda:cloudfrontlogs';
@@ -157,14 +157,14 @@ function parseEvents(fullKey, buf) {
       const source = fullKey;
 
       const lines = data.toString().split(/\r?\n/);
-      for(let i=0;i<lines.length; i++) {
+      for (let i = 0; i < lines.length; i++) {
         const _raw = lines[i].trim();
         if (_raw.length) {
           const e = { _raw, origin, source, region, elb_ip };
           let m = ELB_PARSE_REGEX.exec(_raw);
           let timestampIdx = 0;
           let fields = ELB_FIELDS;
-          if(!m) { // try ALB
+          if (!m) { // try ALB
             m = ALB_PARSE_REGEX.exec(_raw);
             timestampIdx = 1;
             fields = ALB_FIELDS;
@@ -173,10 +173,10 @@ function parseEvents(fullKey, buf) {
           if (m) {
             const tParts = m[timestampIdx + 1].split('.');
             // handle parsing of more than just milliseconds manually
-            e._time = Date.parse(`${tParts[0]}Z`)/1000 + Number(`0.${tParts[1].substr(0, tParts[1].length - 1)}`);
-            for (let k=0; k<fields.length && k<m.length; k++) {
+            e._time = Date.parse(`${tParts[0]}Z`) / 1000 + Number(`0.${tParts[1].substr(0, tParts[1].length - 1)}`);
+            for (let k = 0; k < fields.length && k < m.length; k++) {
               if (k !== timestampIdx) {
-                e[fields[k]] = asNumberIfPossible(m[k+1]);
+                e[fields[k]] = asNumberIfPossible(m[k + 1]);
               }
             }
           }
@@ -192,8 +192,8 @@ function parseEvents(fullKey, buf) {
       const source = fullKey;
       const origin = 'lambda:cloudtrail';
       // not need to add region, it is already in playload as awsRegion
-      for (let i=0; i<records.length; i++) {
-        events.push({ _time: Date.parse(records[i].eventTime)/1000, source, origin, _raw: JSON.stringify(records[i]) });
+      for (let i = 0; i < records.length; i++) {
+        events.push({ _time: Date.parse(records[i].eventTime) / 1000, source, origin, _raw: JSON.stringify(records[i]) });
       }
       return events;
     }
@@ -294,5 +294,4 @@ exports.handler = (event, context, callback) => {
   Promise.all(proms)
     .then(() => callback(), callback);
 };
-
 
